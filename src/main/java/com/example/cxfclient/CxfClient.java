5f8702c2ee71537abf8ca2362ec382331f3435ab
chenye <1062684920@qq.com>
2019-07-23 12:14:05 +0800

    添加说明

--- src/main/java/com/example/cxfclient/CxfClient.java
+++ src/main/java/com/example/cxfclient/CxfClient.java
@@ -3,6 +3,8 @@ package com.example.cxfclient;
 import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 
+import javax.xml.namespace.QName;
+
 public class CxfClient {
 
     public static void main(String[] args) {
@@ -27,12 +29,15 @@ public class CxfClient {
         // 创建动态客户端
         JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
         Client client = dcf.createClient(address);
+        //指定请求的命名空间和方法名(根据情况使用,invoke直接传方法名称也可以)
+        QName name = new QName("http://server.cxfserver.example.com/", "getUser");
+
         // 需要密码的情况需要加上用户名和密码
         // client.getOutInterceptors().add(new ClientLoginInterceptor(USER_NAME, PASS_WORD));
         Object[] objects = new Object[0];
         try {
             // invoke("方法名",参数1,参数2,参数3....);
-            objects = client.invoke("getUser", "1");
+            objects = client.invoke(name, "2");
             System.out.println("返回数据:" + objects[0]);
         } catch (java.lang.Exception e) {
             e.printStackTrace();
